13f3e6c226ac94f5ecccdf2c8830bb3be27351a1
Sarthak Khattar <sarthakoct@gmail.com>
2024-10-03 03:20:50 -0500

    added exit, cd, ls code

--- p3/solution/wsh.c
+++ p3/solution/wsh.c
@@ -1,8 +1,11 @@
+#include <linux/limits.h>
 #define _GNU_SOURCE
 
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <dirent.h>
 #include "wsh.h"
 
 #define DELIM " "
@@ -10,18 +13,45 @@
 #define MIN_TOKEN_LIST_SIZE 5
 #define TOTAL_BUILTINS 7
 
-typedef struct command {
-    int id;
-    int (*cmd)(char* params);
-} command;
+int exec_cmd(size_t argc, char **args) {
+    /*printf("INSIDE exec_cmd\n");*/
+    /*printf("argc: %zu\n", argc);*/
+    /*for (size_t i = 0; i < argc; i ++) {*/
+    /*    printf("%s ", args[i]);*/
+    /*}*/
+    /*printf("\n");*/
+    char *builtins[TOTAL_BUILTINS] = {
+        EXIT,
+        CD,
+        EXPORT,
+        LOCAL,
+        VARS,
+        HISTORY,
+        LS
+    };
 
-int (*wsh_exit)(void);
-int (*wsh_cd)(char* dir);
-int (*wsh_export)(char* var, char* val);
-int (*wsh_local)(char* var, char* val);
-int (*wsh_vars)(void);
-int (*wsh_history)(void);
-int (*wsh_ls)(void);
+    int (*builtin_fn[TOTAL_BUILTINS])(size_t, char**) = {
+        &wsh_exit,
+        &wsh_cd,
+        &wsh_export,
+        &wsh_local,
+        &wsh_vars,
+        &wsh_history,
+        &wsh_ls,
+    };
+
+    size_t i = 0;
+    if (argc == 0) return -1;
+    for (; i < TOTAL_BUILTINS; i++) {
+        /*printf("Len: %zu, Builtin: %s\n", strlen(builtins[i]), builtins[i]);*/
+        /*printf("Len: %zu, Cmd: %s\n", strlen(args[0]), builtins[i]);*/
+        if (strcmp(args[0], builtins[i]) == 0) {
+            /*printf("%s Matched!\n", builtins[i]);*/
+            return ((*builtin_fn[i])(argc, args));
+        }
+    }
+    return -1;
+}
 
 // free string/ string lists
 void freev(void **ptr, int len, int free_seg) {
@@ -30,20 +60,10 @@ void freev(void **ptr, int len, int free_seg) {
     if (free_seg) free(ptr);
 }
 
-// determine which program to run
-int parse(char** tokenList, size_t tokenCnt) {
-    /*printf("Count: %zu\n", tokenCnt);*/
-    /*printf("Tokens: ");*/
-    for (size_t i = 0; i < tokenCnt; i++) {
-        printf("%s ", tokenList[i]);
-    }
-    return 0;
-}
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     // batch mode
     if (argc == 2) {
-        printf("BATCH MODE\n");
         const char *scriptFile = argv[1];
         FILE *script = fopen(scriptFile, "r");
         if (script == NULL) {
@@ -60,10 +80,7 @@ int main(int argc, char* argv[]) {
     }
     // interactive mode
     else if (argc == 1) {
-        printf("INTERACTIVE MODE\n");
         printf("wsh> ");
-
-        command builtin[TOTAL_BUILTINS];
 
         char **tokenList = malloc(MIN_TOKEN_LIST_SIZE * sizeof(char*));
         if (tokenList == NULL){
@@ -80,13 +97,17 @@ int main(int argc, char* argv[]) {
             char *token = strtok(line, DELIM);
             tokenCnt = 0;
             while (token != NULL) {
+                // strip newline char
+                char *ptr = NULL;
+                if ((ptr = strchr(token, '\n'))) *ptr = '\0';
+
                 size_t tokenSize = strlen(token);
                 if (tokenCnt >= tokenListSize) {
                     tokenListSize *= 2;
                     tokenList = reallocarray(tokenList, tokenListSize, sizeof(char*));
                 }
                 tokenList[tokenCnt] = malloc(tokenSize + 1);
-                if (tokenList[tokenCnt] == NULL){
+                if (tokenList[tokenCnt] == NULL) {
                     perror("malloc: failed to allocate memory for tokens\n");
                     freev((void*)tokenList, tokenListSize-1, 1);
                     free(line);
@@ -95,13 +116,12 @@ int main(int argc, char* argv[]) {
 
                 strcpy(tokenList[tokenCnt], token);
                 tokenCnt++;
-
                 token = strtok(NULL, DELIM);
             }
-            if (parse(tokenList, tokenCnt) != 0) {
+            if (exec_cmd(tokenCnt, tokenList) != 0) {
                 freev((void*)tokenList, tokenListSize-1, 1);
                 free(line);
-                break;
+                return -1;
             }
             printf("wsh> ");
         }
@@ -113,5 +133,62 @@ int main(int argc, char* argv[]) {
         return -1;
     }
     
+    return 0;
+}
+
+// Usage: exit
+int wsh_exit(size_t argc, char** args) {
+    if (argc != 1) return -1;
+    freev((void*)args, argc-1, 1);
+    exit(0);
+}
+
+// Usage: cd <dir-path>
+int wsh_cd(size_t argc, char** args) {
+    if (argc != 2) return -1;
+    if (chdir(args[1]) != 0) return -1;
+    return 0;
+}
+
+int wsh_export(size_t argc, char** args) {
+    if (argc != 1 || args == NULL) return -1;
+    return 0;
+}
+
+int wsh_local(size_t argc, char** args) {
+    if (argc != 1 || args == NULL) return -1;
+    return 0;
+}
+
+int wsh_vars(size_t argc, char** args) {
+    if (argc != 1 || args == NULL) return -1;
+    return 0;
+}
+
+int wsh_history(size_t argc, char** args) {
+    if (argc != 1 || args == NULL) return -1;
+    return 0;
+}
+
+
+// Usage: ls
+int wsh_ls(size_t argc, char** args) {
+    if (argc != 1 || args == NULL) return -1;
+
+    char path[PATH_MAX];
+    if (getcwd(path, sizeof(path)) == NULL) return -1;
+
+    struct dirent **names;
+    int n;
+    n = scandir(path, &names, NULL, alphasort);
+    int i = 0;
+    char *pre = ".";
+    while (i < n){
+        if (strncmp(pre, names[i]->d_name, strlen(pre)) != 0)
+            printf("%s\n", names[i]->d_name);
+        free(names[i]);
+        i++;
+    }
+    free(names);
     return 0;
 }
--- p3/solution/wsh.h
+++ p3/solution/wsh.h
@@ -5,3 +5,23 @@
 #define WSH_vars    4
 #define WSH_history 5
 #define WSH_ls      6
+
+// built-in commands
+#define EXIT    "exit"
+#define CD      "cd"
+#define EXPORT  "export"
+#define LOCAL   "local"
+#define VARS    "vars"
+#define HISTORY "history"
+#define LS      "ls"
+
+int wsh_exit(size_t argc, char** args);
+int wsh_cd(size_t argc, char** args);
+int wsh_export(size_t argc, char** args);
+int wsh_local(size_t argc, char** args);
+int wsh_vars(size_t argc, char** args);
+int wsh_history(size_t argc, char** args);
+int wsh_ls(size_t argc, char** args);
+
+
+#define NELEM(x) (sizeof(x)/sizeof((x)[0]))
